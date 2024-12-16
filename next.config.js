1333cb5596b59e612032fd3804740615a6602966
markbudos <markbudos@gmail.com>
2024-12-15 23:12:53 -0800

    cors

--- next.config.js
+++ next.config.js
@@ -2,11 +2,27 @@ const withMarkdoc = require('@markdoc/next.js')
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async headers() {
+    return [
+        {
+            // matching all API routes
+            source: "/markdown/:path*",
+            headers: [
+                { key: "Access-Control-Allow-Credentials", value: "true" },
+                { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
+                { key: "Access-Control-Allow-Methods", value: "GET" },
+                { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+            ]
+        }
+    ]
+  },  
   reactStrictMode: true,
   pageExtensions: ['js', 'jsx', 'md'],
   experimental: {
     scrollRestoration: true,
   },
 }
+
+
 
 module.exports = withMarkdoc()(nextConfig)
